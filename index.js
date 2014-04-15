9fdf5efb8d0abe466896003301f85903ca9e4b3e
Massimiliano Cannarozzo <maxcanna@gmail.com>
2014-04-16 00:52:38 +0200

    Better routing rules

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var app = express();
 var raiapi = require('./raiapi.js');
+
 var port = Number(process.env.PORT || 8080);
 
-app.get('/',function(req,res){
-	res.send('');
+app.enable('trust proxy')
+app.disable('x-powered-by');
+app.set('title', 'Rai API');
+
+app.use(function(req,res,next){
+	console.log('IP:'+req.ip);
+	console.log('X-Mashape-User:'+req.get('X-Mashape-User'));
+	console.log('X-Mashape-Subscription:'+req.get('X-Mashape-Subscription'));
+	console.log(req.headers);
+
+	if(req.get('X-Mashape-Proxy-Secret') != 'x9nH57BIII9F5bbdYoW3TAcaZYF1Mu'){
+		//res.send(401,{error: 'Unauthorized'});
+	}
+	next();
 });
 
 //Canali
@@ -19,7 +32,8 @@ app.get('/canali/:canale/programmi/:programma/qualita', raiapi.handleRequest);
 //Risorsa
 app.get('/canali/:canale/programmi/:programma/qualita/:qualita/:action', raiapi.handleRequest);
 
-app.disable('x-powered-by');
-app.set('title', 'Rai API');
+app.use(function(req,res,next){
+	res.send('');
+});
 
 app.listen(port);
