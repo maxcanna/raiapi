09d03f3e3ab958bfc68c56568eac9c72876c842d
Massimiliano Cannarozzo <maxcanna@gmail.com>
2014-04-25 12:29:37 +0200

    Added country check

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var raiapi = require('./raiapi.js');
+var request = require('request');
 
 var port = Number(process.env.PORT || 8080);
 
@@ -14,10 +15,27 @@ app.use(function(req,res,next){
 	console.log('X-Mashape-Subscription:'+req.get('X-Mashape-Subscription'));
 	console.log(req.headers);
 
-	if(req.get('X-Mashape-Proxy-Secret') != 'x9nH57BIII9F5bbdYoW3TAcaZYF1Mu'){
-		//res.send(401,{error: 'Unauthorized'});
-	}// else 
-	next();
+	//if(req.get('X-Mashape-Proxy-Secret') != 'x9nH57BIII9F5bbdYoW3TAcaZYF1Mu'){
+		//res.send(403, {error: 'Forbidden'});
+	//} else {
+		var options = {
+			headers: {'X-Mashape-Authorization': 'aidrLMAQg2x7xqMVUttS6HmWFfBOueRc'},
+			url : 'https://geoip.p.mashape.com/country?ip='+req.ip+'&format=json&lite=true'
+		};
+	
+		request.get(options, function(error, response, body){
+			if(error || response.statusCode != 200) {
+				res.send(500, {error: 'Hey! Where are you from?!'});
+			} else {
+				console.log('COUNTRY:'+body);
+				if(body.toUpperCase().indexOf('ITALY') > -1){
+					 next();
+				} else {
+					res.send(403, {error: 'Forbidden country'});
+				}
+			}
+		});
+	//}
 });
 
 //Canali
@@ -35,7 +53,7 @@ app.get('/canali/:canale/programmi/:programma/qualita/:qualita/:action', raiapi.
 app.use(function(err,req,res,next){
 	if(err){
 	  console.error(err.stack);
-	  res.send(500, { error: 'Aw, Snap!'});
+	  res.send(500, {error: 'Aw, Snap!'});
 	} else next();
 });
 
