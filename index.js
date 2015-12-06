360fbe337ee9fcc5389b61a320fd333fbca58a85
Massimiliano Cannarozzo <maxcanna@gmail.com>
2015-12-06 19:09:47 +0100

    Remove geofence

--- index.js
+++ index.js
@@ -29,37 +29,10 @@ if (process.env['ENV'] != 'development') {
             ' - X-Mashape-Subscription: ' + req.get('X-Mashape-Subscription'));
 
         const proxy_secret = req.get('X-Mashape-Proxy-Secret');
-        if (proxy_secret == 'e3b3f56615d1e5f2608d2f1130a7ef54') {//md5('override')
-            next(); //Skip GeoIP
-        } else if (proxy_secret != 'x9nH57BIII9F5bbdYoW3TAcaZYF1Mu') {
+        if (proxy_secret != 'x9nH57BIII9F5bbdYoW3TAcaZYF1Mu') {
             const eF = new Error('Forbidden');
             eF.status = 403;
             next(eF);
-        } else {
-            var options = {
-                headers: {
-                    'X-Mashape-Key': 'CJKhzsHwm7mshRdKfeHO2nlYIcVep1OaBQAjsnjSCmncdyqi9O'
-                },
-                json: true,
-                url: 'https://community-telize-json-ip-and-geoip.p.mashape.com/geoip/' + req.ip
-            };
-
-            request.get(options, function (error, response, body) {
-                if (error || response.statusCode != 200) {
-                    const eG = new Error('Hey! Where are you from?!');
-                    eG.status = 500;
-                    next(eG);
-                } else {
-                    console.log('country_code:' + body['country_code']);
-                    if (body['country_code'].toUpperCase().indexOf('IT') > -1) {
-                        next();
-                    } else {
-                        const eF = new Error('Forbidden country');
-                        eF.status = 403;
-                        next(eF);
-                    }
-                }
-            });
         }
     });
 }
