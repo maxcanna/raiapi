5774ca5e00ab74913a42591753467bee51efd40b
Massimiliano Cannarozzo <maxcanna@gmail.com>
2016-05-22 18:16:06 +0200

    Remove moment dependency

--- index.js
+++ index.js
@@ -3,22 +3,11 @@ var express = require('express')
     , app = express()
     , environment = app.get('env') || 'production'
     , development = environment === 'development'
-    , moment = require('moment-timezone')
     , api = require('./routes.js');
 
 app.disable('x-powered-by');
 app.use(require('compression')());
 app.use(require('morgan')('combined'));
-
-app.use(function (req, res, next) {
-    const eod = moment().tz('GMT').endOf('day');
-    res.set({
-        'Cache-Control': 'private, max-age=' + eod.diff(moment().tz('GMT'), 'seconds'),
-        'Last-Modified': moment().tz('GMT').startOf('day').format('ddd, DD MMM YYYY HH:mm:ss z'),
-        'Expires': eod.format('ddd, DD MMM YYYY HH:mm:ss z')
-    });
-    next();
-});
 
 app.use(api);
 
