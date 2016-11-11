f787ce7ecfea96a864d74ec038fec59e1bc2ced4
Massimiliano Cannarozzo <maxcanna@gmail.com>
2016-11-11 11:09:49 +0100

    Move hookfiles config to yml

--- test.sh
+++ test.sh
@@ -9,14 +9,14 @@ node clear_redis.js
 
 printf "\033[34m\n* Running tests with Redis cache empty\n\033[0m\n"
 
-node_modules/.bin/dredd --hookfiles=test/dreddHooks.js
+node_modules/.bin/dredd
 
 printf "\033[34m\n* Running tests with Redis cache full\n\033[0m\n"
 
-node_modules/.bin/dredd --hookfiles=test/dreddHooks.js
+node_modules/.bin/dredd
 
 printf "\033[34m\n* Running tests with Redis cache not available\n\033[0m\n"
 
 unset REDISCLOUD_URL
 
-node_modules/.bin/dredd --hookfiles=test/dreddHooks.js
+node_modules/.bin/dredd
