27462dc8f9f4fee9aa6447bffdbb520942156e63
Massimiliano Cannarozzo <maxcanna@gmail.com>
2018-07-08 16:30:57 +0200

    Use yarn

--- test.sh
+++ test.sh
@@ -9,14 +9,14 @@ node clear_redis.js
 
 printf "\033[34m\n* Running tests with Redis cache empty\n\033[0m\n"
 
-node_modules/.bin/dredd
+yarn dredd
 
 printf "\033[34m\n* Running tests with Redis cache full\n\033[0m\n"
 
-node_modules/.bin/dredd
+yarn dredd
 
 printf "\033[34m\n* Running tests with Redis cache not available\n\033[0m\n"
 
 unset REDISCLOUD_URL
 
-node_modules/.bin/dredd
+yarn dredd
