aae0f9cf84435951391ba9ae7cae27acd4f34a86
Massimiliano Cannarozzo <maxcanna@gmail.com>
2018-11-25 17:13:33 +0100

    Add app component

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import { Router } from 'preact-router';
+import Header from '../Header';
+import Home from '../Home';
+import NotFound from '../NotFound';
+
+export default () => (
+	<div id="app">
+		<Header />
+		<Router>
+			<Home path="/" />
+			<NotFound default />
+		</Router>
+	</div>
+);
