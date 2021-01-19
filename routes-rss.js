ed3d30b70b95cfa3be0e736376c5bdceb407ee84
Massimiliano Cannarozzo <maxcanna@gmail.com>
2021-01-19 17:18:49 +0100

    Add dateTag parameter

--- routes-rss.js
+++ routes-rss.js
@@ -7,7 +7,7 @@ const api = new RaiApi();
 const router = require('express').Router();
 const dateValidator = require('./validator-date');
 const cacheHeaders = require('./middleware-headers-cache');
-const moment = require('moment-timezone');
+const moment = require('moment-timezone').tz.setDefault('Europe/Rome');
 
 let canali = {};
 
@@ -19,18 +19,20 @@ router.use(cacheHeaders);
 //RSS
 router.get(/^\/canali\/(\d+)\.xml/, (req, res, next) => {
     const { params: { 0: canale }, hostname, url, query: { data } } = req;
-    const utc = moment.utc();
+    const m = moment(data);
+
     api.getAll(canale, data)
         .then(programmi => res.set({
             'Content-Type': 'text/xml',
             'Cache-Control': 'public, max-age=86400',
-            'Last-Modified': utc.startOf('day').format('ddd, DD MMM YYYY HH:mm:ss [GMT]'),
-            Expires: utc.endOf('day').format('ddd, DD MMM YYYY HH:mm:ss [GMT]'),
+            'Last-Modified': m.startOf('day').format('ddd, DD MMM YYYY HH:mm:ss [GMT]'),
+            Expires: m.endOf('day').format('ddd, DD MMM YYYY HH:mm:ss [GMT]'),
         }).render('rss.ejs', {
             programmi,
             hostname,
             url,
             canale: canali[canale].name,
+            dateTag: m.format('YYYY.MM.DD'),
         }))
         .catch(error => next(error))
 });
