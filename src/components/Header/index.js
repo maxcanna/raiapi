918606741b7ad7bceba243b7fbcf15ac80b11fa5
Massimiliano Cannarozzo <maxcanna@gmail.com>
2021-04-17 20:54:43 +0200

    Migrate Header to rmwc

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,32 @@
 import { route } from 'preact-router';
-import TopAppBar from 'preact-material-components/TopAppBar';
-import 'preact-material-components/TopAppBar/style.css';
-import style from './style';
+import {
+    TopAppBar,
+    TopAppBarSection,
+    TopAppBarActionItem,
+    TopAppBarRow,
+    TopAppBarTitle,
+    TopAppBarNavigationIcon,
+    TopAppBarFixedAdjust,
+} from 'rmwc';
+import '@rmwc/top-app-bar/styles';
 
 export default () => (
-    <TopAppBar className={`topappbar ${style.sticky}`} onNav={() => route('/')}>
-        <TopAppBar.Row>
-            <TopAppBar.Section align-start>
-                <TopAppBar.Icon navigation>live_tv</TopAppBar.Icon>
-                <TopAppBar.Title>Rai API</TopAppBar.Title>
-            </TopAppBar.Section>
-            <TopAppBar.Section align-end>
-                <TopAppBar.Icon
-                    href="https://github.com/maxcanna/raiapi"
-                    title="GitHub"
-                    target="_new"
-                >
-                    merge_type
-                </TopAppBar.Icon>
-            </TopAppBar.Section>
-        </TopAppBar.Row>
-    </TopAppBar>
+    <>
+        <TopAppBar fixed onNav={() => route('/')} >
+            <TopAppBarRow>
+                <TopAppBarSection alignStart>
+                    <TopAppBarNavigationIcon icon="live_tv" />
+                    <TopAppBarTitle>Rai API</TopAppBarTitle>
+                </TopAppBarSection>
+                <TopAppBarSection alignEnd>
+                    <TopAppBarActionItem
+                        icon="archive"
+                        onClick={() => location.href="https://github.com/maxcanna/raiapi"}
+                        title="GitHub"
+                    />
+                </TopAppBarSection>
+            </TopAppBarRow>
+        </TopAppBar>
+        <TopAppBarFixedAdjust />
+    </>
 );
