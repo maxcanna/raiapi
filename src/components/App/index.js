692785deb1192684d71428d908cd4964fb169ac5
Massimiliano Cannarozzo <maxcanna@gmail.com>
2021-04-17 20:28:06 +0200

    Migrate App to rmwc

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,13 +2,14 @@ import { Router } from 'preact-router';
 import Header from '../Header';
 import Home from '../Home';
 import NotFound from '../NotFound';
+import '@rmwc/theme/styles';
 
 export default () => (
-    <div id="app">
+    <>
         <Header />
         <Router>
             <Home path="/" />
             <NotFound default />
         </Router>
-    </div>
+    </>
 );
