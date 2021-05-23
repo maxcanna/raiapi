fb3a604e5acb2ab62d4eabbe789f12e03442a259
Massimiliano Cannarozzo <maxcanna@gmail.com>
2021-05-23 16:16:12 +0200

    Set base URL

--- raiapi.js
+++ raiapi.js
@@ -4,6 +4,7 @@
 /* eslint-env node */
 const ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36';
 const axios = require('axios').create({
+    baseURL: 'https://www.raiplay.it',
     headers: {
         'User-Agent': ua,
     },
